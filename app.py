dcc99aa2ceecc1df927723cb3cc26cd3faee8ac7
Jahnavi Murali <109456718+jahnavimurali@users.noreply.github.com>
2024-03-31 17:38:57 +0530

    firebase-auth added to login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, jsonify, send_from_directory, redirect,session
 
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, auth
 
 app = Flask(__name__)
 
@@ -13,9 +13,29 @@ db = firestore.client()
 
 @app.route('/')
 def index():
-    return render_template('dashboard.html')
+    return render_template('login_signup.html')
 
 # Add other routes and functionalities as needed
+
+@app.route('/verifyToken', methods=['POST'])
+def verify_token():
+    global email
+    data = request.get_json()
+    id_token = data.get('idToken')
+    email = data.get('email')
+    print(data)
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        return jsonify({'success': True}), 200
+    except Exception as e:
+        return jsonify({'error': 'Failed to verify token', 'details': str(e)}), 500
+
+@app.route('/dashboard')
+def dashboard():
+        global email
+        print(email)
+        return render_template('dashboard.html', mail=email)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
